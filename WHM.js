0d4b27c81f5c9caa0b247afc7f409e142ff655b8
Juras122 <juras1.juras1@gmail.com>
2025-10-01 17:26:11 +0200

    Update WHM.js

--- WHM.js
+++ WHM.js
@@ -1,12 +1,17 @@
 const loggedInUserId = sessionStorage.getItem('loggedInUserId');
 
+console.log(${ime});
+
 // Preverite, ali je uporabnik prijavljen
 if (!loggedInUserId) {
     // Če uporabnik ni prijavljen, ga preusmerite na stran za prijavo
     window.location.href = 'Prijava.html';
 }
 
+
+
 //Posodobiko URL z ID-jem uporabnika
 const url = new URL(window.location.href);
 url.searchParams.set('id', loggedInUserId);
+
 window.history.replaceState({}, '', url);
