ada553a23deef8c7e9d765bed9428b9fc3148871
Juras122 <juras1.juras1@gmail.com>
2025-10-03 23:22:57 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ app.get('/api/rdn', async (req, res) => {
     try {
         // Corrected SQL query to select all required columns
         const result = await pool.query(
-            'SELECT "serijska_stevilka", "lokacija", "vrsta", "material", "rok_izvedbe", "nacrti_url" FROM rdn'
+            'SELECT "serijska", "lokacija", "vrsta", "material", "rok_izvedbe", "nacrti_url" FROM rdn'
         );
 
         if (result.rows.length > 0) {
