55bcd424b5f7d39cfcce3b69677f461cd20dd33a
Juras122 <juras1.juras1@gmail.com>
2025-11-09 19:45:06 +0100

    Update index.js

--- index.js
+++ index.js
@@ -74,7 +74,7 @@ app.get('/api/workhours/:id', async (req, res) => {
     const userId = req.params.id;
 
     try {
-        const result = await pool.query('SELECT * FROM whm WHERE id = $1', [userId]);
+        const result = await pool.query('SELECT * FROM delo_ure WHERE id = $1', [userId]);
         if (result.rows.length > 0) {
             res.json(result.rows);
         } else {
@@ -242,6 +242,7 @@ app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
 //--------------------------------------------------------------------------------------------------------
+
 
 
 
